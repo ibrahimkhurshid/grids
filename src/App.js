5eb8995d3e07b54cc95d5d67044ce663ae7e295a
Ibrahim Khurshid <ibrahimkhurshid94@hotmail.com>
2022-09-26 09:01:52 +0500

    checkpoint_02

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <div class="item">Item 1</div>
+      <div class="item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, assumenda? Delectus quae dignissimos iste, accusantium laudantium saepe tempore atque. Perferendis rerum, sint odit blanditiis eveniet, </div>
       <div class="item">Item 2</div>
       <div class="item">Item 3</div>
       <div class="item">Item 4</div>
       <div class="item">Item 5</div>
       <div class="item">Item 6</div>
       <div class="item">Item 7</div>
-      <div class="item">Item 8</div>
+      <div class="item">Lorem ipsum dolor sit amet cit. Voluptatibus, aliquid.</div>
       <div class="item">Item 9</div>
     </div>
   );
